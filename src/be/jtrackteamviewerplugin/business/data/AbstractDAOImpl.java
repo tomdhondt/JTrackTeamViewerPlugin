89d3a1bba8c90132fff7b5411a821893b023ac36
tomdhondt <tom.dhondt@hotmail.com>
2014-06-08 09:45:19 +0200

    Refactor JtrackTeamViewer Data integration

--- src/be/jtrackteamviewerplugin/business/data/AbstractDAOImpl.java
+++ src/be/jtrackteamviewerplugin/business/data/AbstractDAOImpl.java
@@ -1,22 +1,23 @@
 package be.jtrackteamviewerplugin.business.data;
 
+import java.util.Collections;
 import java.util.List;
+
+import main.java.info.jtrac.exception.JTrackException;
+import main.java.info.jtrac.exception.data.DataDAOException;
 
 import org.apache.log4j.Logger;
 import org.hibernate.HibernateException;
 import org.hibernate.Query;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 
-// extends Hibernatetemplate
-public abstract class AbstractDAOImpl<T> implements IPersistenceDAO<T>{
+public abstract class AbstractDAOImpl<T> implements IPersistenceDAOImpl<T>{
 	/* 
 	 * Instance Members
 	 */
-	protected Class<T> type;
 	protected SessionFactory sessionFactory;
-	private static Logger logger = Logger.getLogger(AbstractDAOImpl.class);
+	protected Class<T> type;
+	private Logger logger = Logger.getLogger(JTrackException.class);
     /**
 	 * Default constructor for the Class
 	 */
@@ -25,162 +26,283 @@ public abstract class AbstractDAOImpl<T> implements IPersistenceDAO<T>{
 	/**
 	 * @return the type
 	 */
-	public Class<T> getType() {
-		return type;
-	}
-	/**
-	 * @return the sessionFactory
-	 */
-	public SessionFactory getSessionFactory() {
-		return sessionFactory;
-	}
-	/**
-	 * @return the transactionManager
-	 */
-	public Transaction getTransaction() {
-		return this.sessionFactory.getCurrentSession().getTransaction();
-	}
-	/**
-	 * @return the session
-	 */
-	public Session getSession() {
-		return this.sessionFactory.getCurrentSession();
-	}
-	/**
-	 * @param type the type to set
-	 */
 	public void setType(Class<T> type) {
 		this.type = type;
 	}
 	/**
-	 * @param sessionFactory the sessionFactory to set
+	 * @return the type
 	 */
-	public void setSessionFactory(SessionFactory sessionFactory) {
-		this.sessionFactory = sessionFactory;
+	public Class<T> getType() {
+		return type;
+	}
+	/**
+	 * Method will find a object in the database based on the criteria of the object.
+	 * @param Object as T
+	 * @return Set<T> as resultSet
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<T> findByCriteria(List<NameQueryParam> list , String namedQuery) throws DataDAOException {
+		List<T> result = null;
+		if(null != namedQuery && null != list){
+			try{
+				this.sessionFactory.getCurrentSession().getTransaction().begin();
+				Query q = this.sessionFactory.getCurrentSession().getNamedQuery(namedQuery);
+				Collections.sort(list);
+				for(NameQueryParam nQp : list){
+					q.setString(nQp.getVarName(), nQp.getValue());
+				}
+				result = q.list();
+				this.sessionFactory.getCurrentSession().getTransaction().commit();			
+			}catch(Exception e){
+				throw new DataDAOException("object.findAll.error",e.getStackTrace());
+			}
+		}
+		return result;
+	}
+	/**
+	 * Method will find a object in the database based on the criteria of the object.
+	 * @param Object as T
+	 * @return Set<T> as resultSet
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Object> findByCriteria(String namedQuery) throws DataDAOException {
+		List<Object> result = null;
+		if(null != namedQuery){
+			try{
+				this.sessionFactory.getCurrentSession().getTransaction().begin();
+				Query q = this.sessionFactory.getCurrentSession().getNamedQuery(namedQuery);
+				result = q.list();
+				this.sessionFactory.getCurrentSession().getTransaction().commit();			
+			}catch(Exception e){
+				throw new DataDAOException("object.findAll.error",e.getStackTrace());
+			}
+		}
+		return result;
+	}
+	/**
+	 * Method will find a object in the database based on the criteria of the object.
+	 * @param Object as T
+	 * @param count as int
+	 * @return Set<T> as resultSet
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<T> findByCriteria(List<NameQueryParam> list , String namedQuery, int count) throws DataDAOException {
+		List<T> result = null;
+		if(null != namedQuery && null != list){
+			try{
+				this.sessionFactory.getCurrentSession().getTransaction().begin();
+				Query q = this.sessionFactory.getCurrentSession().getNamedQuery(namedQuery);
+				Collections.sort(list);
+				for(NameQueryParam nQp : list){
+					q.setString(nQp.getVarName(), nQp.getValue());
+				}
+				if(count <= 0){
+					q.setMaxResults(1);
+				}else{
+					q.setMaxResults(count);
+				}
+				result = q.list();
+				this.sessionFactory.getCurrentSession().getTransaction().commit();			
+			}catch(Exception e){
+				throw new DataDAOException("object.findAll.error",e.getStackTrace());
+			}
+		}
+		return result;
+	}
+	/**
+	 * Method will persist the object in the database
+	 */
+	@Override
+	public void persist(T object) throws DataDAOException {
+		try{
+			this.sessionFactory.getCurrentSession().getTransaction().begin();
+			this.sessionFactory.getCurrentSession().persist(object);
+			this.sessionFactory.getCurrentSession().getTransaction().commit();			
+		}catch(Exception eXe){
+			throw new DataDAOException("object.persist.couldNotPersist", eXe.getStackTrace());
+		}
 	}
 	/**
 	 * Method will find the object by ID
 	 * @param id as int
 	 * @return object of type T
 	 */
-	@Override
 	@SuppressWarnings("unchecked")
-    public T findByID(int id) {
+	@Override
+    public T findByID(long id) throws DataDAOException{
 		/* create a object to pointer the object */
 		T object = null;
 		try{
-			this.getSessionFactory().getCurrentSession().getTransaction().begin();
-			object = (T) this.getSessionFactory().getCurrentSession().get(this.getType(),id);
-			this.getSessionFactory().getCurrentSession().getTransaction().commit();
-		}catch(Exception eXp){
+			this.sessionFactory.getCurrentSession().getTransaction().begin();
+			Query query = this.sessionFactory.getCurrentSession().createQuery("FROM " + this.getType().getName() + " WHERE ID = "+ id);
+			List<T> resultList = query.list();
+			/* set the return object */
+			for(T ob : resultList){
+				object = ob;
+			}
+			this.sessionFactory.getCurrentSession().getTransaction().commit();
+		}catch(Exception eXe){
 			try{
-				this.getSessionFactory().getCurrentSession().getTransaction().rollback();
-				logger.error("Rollback of the findById of the object type : " + this.getType().getName() + "!");
+				this.sessionFactory.getCurrentSession().getTransaction().rollback();
+				throw new DataDAOException("Object.findByID.notObjectFound", eXe.getStackTrace());
 			}catch(HibernateException hEx){
-				logger.error("Rollback of the findById could not be performed!");
+				throw new DataDAOException("Object.findByID.notObjectFound", hEx.getStackTrace());
 			}
 		}
 		return object;
     }
 	/**
-	 * Method will find the object by ID
-	 * @param id as int
-	 * @return object of type T
+	 * Method will return all the objects<T>
+	 * @param type as Class<T>
+	 * @return objects as List<T>
+	 * <li>the list equals null when there could not found a result</li>
+	 * @throws DataDAOException
 	 */
-    @SuppressWarnings("rawtypes")
-	public Object findByID(int id, Class type) {
-		/* create a object to pointer the object */
-		Object object = null;
-		try{
-			this.getSessionFactory().getCurrentSession().getTransaction().begin();
-			this.getSessionFactory().getCurrentSession().createQuery("FROM " + type.getName() + " WHERE id = " + id);
-			this.getSessionFactory().getCurrentSession().getTransaction().commit();
-		}catch(Exception eXp){
-			try{
-				this.getSessionFactory().getCurrentSession().getTransaction().rollback();
-				logger.error("Rollback of the findById of the object type : " + type.getName() + "!");
-			}catch(HibernateException hEx){
-				logger.error("Rollback of the findById could not be performed!");
-			}
-		}
-		return object;
-    }
-	/**
-	 * Method will return the referenced object based on the ID
- 	 * @param type as Class<T>
-	 * @param id as int
-	 * @return object T
-	 */
+	@SuppressWarnings("unchecked")
 	@Override
-	public T getReference(Class<T> type, int id){
-		return this.findByID(id);
-	}
-	/**
-	 * Method will remove the object out the database
-	 * @param entity as T
-	 * @return boolean as success rate
-	 */
-	public boolean remove(int id){
-		boolean success = false;
-		/* check if the object exists */
-		T object = this.findByID(id);
-		/* delete the object in the database */
-		if(null != object){
-			this.getTransaction().begin();
-			Query q = this.getSession().createQuery("DELETE " + this.getType().getName() + " WHERE id = " + id);
-			logger.info("Remove query for the object " + this.getType().getName() + " : " + q.getQueryString());
-			int count = q.executeUpdate();
-			this.getTransaction().commit();
-			if(-1 < count){
-				success = true;
-				logger.info(this.getType().getName() + " with id " + id + " was successful removed in the database!");
-			}
-		}else{
-			logger.info("Object " + this.getType().getName() + " could not be removed." + this.getType().getName() + " wasn't found in the database.");
+	public List<T> findAll() throws DataDAOException{
+		List<T> resultList = null;
+		try{
+			this.sessionFactory.getCurrentSession().getTransaction().begin();
+			Query query = this.sessionFactory.getCurrentSession().createQuery("FROM " + this.getType().getName());
+			resultList = query.list();
+			this.sessionFactory.getCurrentSession().getTransaction().commit();
+		}catch(HibernateException hEx){
+			logger.error(hEx.getMessage());
+			throw new DataDAOException("object.findAll.error",hEx.getStackTrace());
 		}
-		
-		return success;
-	}
-	/**
-	 * Method will remove the object out the database
-	 * @param id as int
-	 * @param type as Class
-	 * @return boolean as success rate
-	 */
-	@SuppressWarnings("rawtypes")
-	public boolean remove(int id, Class type){
-		boolean success = false;
-		/* check if the object exists */
-		Object object = this.findByID(id, type);
-		/* delete the object in the database */
-		if(null != object){
-			this.getTransaction().begin();
-			Query q = this.getSession().createQuery("DELETE " + type.getName() + " WHERE id = " + id);
-			logger.info("Remove query for the object " + type.getName() + " : " + q.getQueryString());
-			int count = q.executeUpdate();
-			this.getTransaction().commit();
-			if(-1 < count){
-				success = true;
-				logger.info(type.getName() + " with id " + id + " was successful removed in the database!");
-			}
-		}else{
-			logger.info("Object " + type.getName() + " could not be removed." + type.getName() + " wasn't found in the database.");
-		}
-		
-		return success;
+		return resultList;
 	}
 	/**
 	 * Method will return all the objects<T>
 	 * @param type as Class<T>
 	 * @return objects as List<T>
+	 * <li>the list equals null when there could not found a result</li>
+	 * @throws DataDAOException
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<T> findAll(int count) throws DataDAOException{
+		List<T> resultList = null;
+		try{
+			this.sessionFactory.getCurrentSession().getTransaction().begin();
+			Query query = this.sessionFactory.getCurrentSession().createQuery("FROM " + this.getType().getName());
+			if(count <=0){
+				query.setMaxResults(1);
+			}else{
+				query.setMaxResults(count);	
+			}
+			resultList = query.list();
+			this.sessionFactory.getCurrentSession().getTransaction().commit();
+		}catch(HibernateException hEx){
+			logger.error(hEx.getMessage());
+			throw new DataDAOException("object.findAll.error",hEx.getStackTrace());
+		}
+		return resultList;
+	}
+	/**
+	 * Method will delete the object out the database
+	 * @param id as String
+	 * @return boolean as success rate
+	 * @throws DataDAOException as Exception
+	 */
+	public boolean delete(long id)throws DataDAOException{
+		boolean success = false;
+		try{
+			this.sessionFactory.getCurrentSession().getTransaction().begin();
+			Query query = this.sessionFactory.getCurrentSession().createQuery("DELETE " + this.getType().getName() + " WHERE ID = "+ id );
+			if(0 < query.executeUpdate()){
+				success = true;
+			}
+			this.sessionFactory.getCurrentSession().getTransaction().commit();
+		}catch(Exception eXe){
+			logger.error(eXe.getMessage());
+			throw new DataDAOException("object.delete.error", eXe.getStackTrace());
+		}
+		return success;
+	}
+	/**
+	 * Method will update the PropertyGroup in the database
+	 * @param object as PropertyGroup
+	 * @return PropertyGroup as object
+	 * <li>null : when the object could not be updated </li>
+	 * @exception DataDAOException when the object that we try to update isn't in the database
+	 * @exception HibernateException when there is a problem communicate with the database
+	 * @exception Exception when there is a global exception
 	 */
 	@Override
-	public List<T> findAll(){
-		this.getTransaction().begin();
-		Query query = this.getSession().createQuery("FROM " + this.getType().getName());
-		@SuppressWarnings("unchecked")
-		List<T> listObject = (List<T>) query.list(); 
-		this.getTransaction().commit();
-		return listObject;
+	public boolean remove(long id) throws DataDAOException {
+		/* method variable */
+		boolean success = false;
+		try{
+			this.sessionFactory.getCurrentSession().getTransaction().begin();
+		    String hql = "UPDATE " + this.getType().getName() + " SET active = :active WHERE ID = :ID";
+		    Query query = this.sessionFactory.getCurrentSession().createQuery(hql);
+		    query.setBoolean("active",false);
+		    query.setLong("ID",id);
+			if(0 < query.executeUpdate()){
+				success = true;
+			}
+			this.sessionFactory.getCurrentSession().getTransaction().commit();
+		}catch(Exception eXe){
+			logger.error(eXe.getMessage());
+			throw new DataDAOException("object.remove.couldNotPersist", eXe.getStackTrace());
+		}
+		/* return the success rate */
+		return success;
+	}
+	/**
+	 * Method will update the object in de database.
+	 * 
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public T update(T object) throws DataDAOException{
+		T updatedObject = null;
+		/* Update the object in the database */
+		try{
+			this.sessionFactory.getCurrentSession().getTransaction().begin();
+			this.sessionFactory.getCurrentSession().saveOrUpdate(object);
+			this.sessionFactory.getCurrentSession().getTransaction().commit();
+		}catch(Exception eXe){
+			logger.error(eXe.getMessage());
+			eXe.printStackTrace();
+			throw new DataDAOException("object.update.couldNotUpdate", eXe.getStackTrace());
+			
+		}
+		/* merge the object */
+		try{
+			this.sessionFactory.getCurrentSession().getTransaction().begin();
+			updatedObject = (T) this.sessionFactory.getCurrentSession().merge(object);
+			this.sessionFactory.getCurrentSession().getTransaction().commit();
+		}catch(Exception eXe){
+			logger.error(eXe.getMessage());
+			throw new DataDAOException("object.merge.couldNotMerge", eXe.getStackTrace());
+		}
+		return updatedObject;
+	}
+	/**
+	 * Method will drop the table out the database
+	 * @return boolean as success rate
+	 */
+	@Override
+	public boolean dropTable() throws DataDAOException{
+		boolean success = false;
+		try{
+			this.sessionFactory.getCurrentSession().getTransaction().begin();
+		    String hql = "DELETE FROM " + this.getType().getName();
+		    Query query = this.sessionFactory.getCurrentSession().createQuery(hql);
+			if(0 < query.executeUpdate()){
+				success = true;
+			}
+			this.sessionFactory.getCurrentSession().getTransaction().commit();
+		}catch(Exception eXe){
+			logger.error(eXe.getMessage());
+			throw new DataDAOException("drop.table.error", eXe.getStackTrace());
+		}
+		return success;
 	}
 }
